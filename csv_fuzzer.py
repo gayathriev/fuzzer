97a9fd5c6053d01f74f5aff2dfbcf17750cdabac
j-he880 <33657736+j-he880@users.noreply.github.com>
2022-07-10 20:08:28 +1000

    Create csv_fuzzer.py
    Added in reading capability for binaries that take in a csv input

---
+++ csv_fuzzer.py
@@ -0,0 +1,42 @@
+from pwn import *
+import csv
+
+def open_process_csv():
+    try:
+        p = process("./" + sys.argv[1])
+        context.log_level = 'debug'
+        return p
+    except:
+        print("invalid binary :(")
+        sys.exit() 
+
+def parse_csv_input():
+
+    data = []
+
+    try:
+        print(sys.argv[2])
+        with open(sys.argv[2], newline='') as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                print(len(row))
+                data.append(row)
+    except Exception as e:
+        print("invalid csv input :^(")
+        print(e)
+        sys.exit() 
+
+    return data
+
+"""gdb.attach('image-viewer','''
+set follow-fork-mode child
+break execve
+continue
+''')"""
+
+p = open_process_csv()
+data = parse_csv_input()
+
+print(data)
+
+p.interactive()
