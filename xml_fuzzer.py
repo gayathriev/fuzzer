938309423fbe02fd195990079e135bf0ba3cd341
Kenneth <kenneth.mejico@outlook.com>
2022-07-15 00:55:50 +1000

    more xml fuzz work - injecting fmtstr and overflow

--- xml_fuzzer.py
+++ xml_fuzzer.py
@@ -1,9 +1,17 @@
+import io
+import sys
+import os
 import random
 import copy
 import xml
 import xml.etree.ElementTree as ET
 
 from pwn import *
+
+MAX_BUF = 0x100
+
+def tree_to_string(tree):
+    return ET.tostring(tree).decode()
 
 def read_xml(filename):
     f = open(filename)
@@ -33,13 +41,31 @@ def breed_child(child, parent, xml):
 
     return root
 
+def bit_flip(byte):
+    return byte ^ random.choice([1, 2, 4, 8, 16, 32, 64, 128])
+
+def inject_overflow(xml):
+    root = copy.deepcopy(xml)
+    for c in root.iter('a'):
+        c.set("href", "https://" + "A" * MAX_BUF + ".com")
+    return root
+
+def inject_fstring(xml):
+    root = copy.deepcopy(xml)
+    for c in root.iter('a'):
+        c.set("href", "https://" + "%s" * MAX_BUF + ".com")
+    return root
+
 #################################
 ###     TEST
 #################################
 def test_payload(binary_file, xml):
     p = process('./' + binary_file)
-    p.send(xml)
-    mess = p.recvlines(2, timeout=0.1)
+    payload = bytes(xml, 'utf-8')
+    # Hardcoded fread exit, fread not terminating otherwise
+    payload += b'A' * (0x270e - len(payload))
+    p.send(payload)
+    mess = p.recvlines(2, timeout=0.2)
     print(mess)
     p.close()
 
@@ -52,7 +78,9 @@ def generate_input(xml):
     # Original input
     print("original")
     print("----------------------")
-    yield ET.tostring(xml).decode()
+    input = tree_to_string(xml)
+    print(input)
+    yield input
 
     # Add child to parent rand times
     print("breadthwise add")
@@ -67,9 +95,17 @@ def generate_input(xml):
         for child in xml:
             yield ET.tostring(breed_child(child, parent, xml)).decode()
 
-    # Link overflow
+    # Inject fstring
+    print("inject fstring")
+    print("----------------------")
+    input = tree_to_string(inject_fstring(xml))
+    yield input
 
-    # Link fstring
+    # Overflow link
+    print("overflow link")
+    print("----------------------")
+    input = tree_to_string(inject_overflow(xml))
+    yield input
 
     # Content int overflow (2 ** 31)
 
@@ -79,16 +115,46 @@ def generate_input(xml):
 
     # Child name fstring
 
+    # Bit flips
+
+    # Byte flips
+
+    # Known ints
+
+    # Repeated parts
+
+    # Keyword extraction
+
+    # Arithmetic
+
+    # Coverage based mutations
+
+    # Buffer overflow tag names
+
+    # Buffer overflow tag properties
+
+    # Tag Name to Format String
+
+    # Tag Properties to Format String
+
+    # Big print
+
+    # Properties --> bad ints
+
+    # Content --> bad ints
+
+    # Bit shift
+
+    # Node Shuffle
 
 #################################
 ###     MAIN STUFF
 #################################
-def xml_fuzzer(binary_file, input, loops=10):
+def xml_fuzzer(binary_file, input):
     xml = read_xml(input)
     for test in generate_input(xml):
         try:
             test_payload(binary_file, test)
         except Exception as e:
             print(e)
-            
         
