2c39d3024023cb8017fe47d19e4d9d21e8067ed8
j-he880 <33657736+j-he880@users.noreply.github.com>
2022-07-17 12:01:42 +1000

    Update csv_fuzzer.py
    Added ability to take in arguments for the data and binary files

--- csv_fuzzer.py
+++ csv_fuzzer.py
@@ -13,35 +13,40 @@ class Payload(Enum):
     NUM_ZERO = 8
     NUM_NEGATIVE = 9
     NUM_LARGE = 10
-    NUM_FLOAT = 11
+    NUM_FLOAT = 11 
 
-
-def open_process_csv():
+# 
+#   Open process, process location is a string
+#
+def open_process_csv(p):
     try:
-        p = process("./" + sys.argv[1])
-        context.log_level = 'debug'
-        return p
+        return process('./' + p)
     except Exception as e:
         print("invalid binary :(")
         print(e)
         sys.exit() 
 
-def parse_csv_input():
+# 
+#   Open process, data location is a string
+#
+def parse_csv_input(path):
 
     data = []
-
     try:
-        print(sys.argv[2])
-        with open(sys.argv[2], newline='') as csv_file:
+        print("this is a list?")
+        print(path)
+        with open(path, newline='') as csv_file:
+            print(csv_file)
             csv_reader = csv.reader(csv_file, delimiter=',')
             for row in csv_reader:
                 print(len(row))
                 data.append(row)
     except Exception as e:
-        print("invalid csv input :^(")
+        print("invalid data :(")
         print(e)
         sys.exit() 
-
+    print("this is the data extracted:")
+    print(data)
     return data
 
 # send the header of the csv file as one line.
@@ -50,9 +55,20 @@ def send_header(process,data):
     for entry in data[0]:
         payload += entry
         payload += ','
-    process.sendline(payload[:-1])
+    return payload
 
-def empty_payload(process, data):
+def generate_header(process,data):
+    payload = b''
+    for entry in data[0]:
+        payload += bytes(entry,'utf-8')
+        payload += b','
+
+    payload = payload[:-1]
+    payload += b'\n'
+
+    return payload
+
+def empty_payload(process, data, send_header):
     delimiter = len(data[0])
     if(delimiter < 0):
         delimiter = 0
@@ -60,31 +76,48 @@ def empty_payload(process, data):
     print("delimiter is " + str(delimiter))
     i = 0
 
+    payload = b''
+
+    if(send_header == True):
+        payload = generate_header(process,data)
+    
+
+    payload += ((b','*delimiter) + b'\n')*len(data)
+
     while 1==1:
-        process.sendline(b','*delimiter)
+        process.sendline(payload)
         i+=1
         print(i)
 
-def zero_payload(process, data):
+def zero_payload(process, data, send_header):
     delimiter = len(data[0])
     if(delimiter < 0):
         delimiter = 0
 
     print("delimiter is " + str(delimiter))
     i = 0
+
+    payload = b''
+
+    if(send_header == True):
+        payload = generate_header(process,data)
+
+
 
     string = b'0,' * delimiter
     string = string[:-1]
+    string += b'\n'
+    string *= len(data)
 
-    send_header(process,data)
+    payload += string
 
     while 1==1:
 
-        process.sendline(string)
+        process.sendline(payload[:-1])
         i+=1
         print(i)
 
-def negative_payload(process, data):
+def negative_payload(process, data, send_header):
     delimiter = len(data[0])
     if(delimiter < 0):
         delimiter = 0
@@ -92,17 +125,27 @@ def negative_payload(process, data):
     print("delimiter is " + str(delimiter))
     i = 0
 
+    payload = b''
+
+    if(send_header == True):
+        payload = generate_header(process,data)
+
     string = b'-1,' * delimiter
     string = string[:-1]
+    string += b'\n'
+    string *= len(data)
+
+
+    payload += string
 
     while 1==1:
 
-        process.sendline(string)
+        process.sendline(payload)
         i+=1
         print(i)
 
 
-def large_payload(process, data, size):
+def large_payload(process, data, size, send_header):
     delimiter = len(data[0])
     if(delimiter < 0):
         delimiter = 0
@@ -124,9 +167,9 @@ def large_payload(process, data, size):
 
 #TODO: add detection of when the program is just running and not resolving
 
-p = open_process_csv()
-data = parse_csv_input()
-print(data)
+# Program will take in two arguments
+p = open_process_csv(sys.argv[1])
+data = parse_csv_input(sys.argv[2])
 
-zero_payload(p,data)
+zero_payload(p,data,True)
 p.interactive()
