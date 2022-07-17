be11e1cf70bb8492b5a624f93d13d4a9bc5cd479
Gayathrie Vijayalingam <gayathrie@ubuntu.com>
2022-07-17 00:00:40 -0700

    bad.txt

--- support/log_crash.py
+++ support/log_crash.py
@@ -6,5 +6,6 @@ Simple module writes the bad input to a file
 
 def log_crash(crash_input):
     str_input = str(crash_input)
+    str_input += '\n'
     with open('./bad.txt', 'w') as crash:
         crash.write(str_input)
