7a5fbfdfb26b2b1b76fd49e75223a0a6a6bb107d
Kenneth <kenneth.mejico@outlook.com>
2022-08-07 16:17:29 +1000

    running but all loops are throwing exceptions

--- modes/jpeg_fuzzer.py
+++ modes/jpeg_fuzzer.py
@@ -27,7 +27,8 @@ def mutate_type(jpeg):
         if type == 2: # insert special bytes
             res[key] = insert_special_bytes(jpeg)
         if type == 3: # Use the 64 bit method
-            res[key] = b64_fuzz(jpeg)
+            pass
+            #res[key] = b64_fuzz(jpeg)
         if type == 4: # flip random bits
             res[key] = bit_flip(jpeg)
         if type == 5: # float
@@ -176,13 +177,14 @@ def jpeg_fuzzer(binary_file, input):
     print("============== running jpeg fuzzer ==============")
     for i in range(0, LOOPS):
         try:
-            res = mutate(jpeg)
+            #res = mutate(jpeg)
             # print('===', res['len'], '===', len(res['input']))
-            test_payload(binary_file, res)
+            #test_payload(binary_file, res)
             
-            res = mutate_type(jpeg)
+            yield mutate_type(jpeg)
             # print('===', res['len'], '===', len(res['input']))
-            test_payload(binary_file, res)
+            #test_payload(binary_file, res)
         except Exception as e:
-            print(e)
+            pass
+            #print(e)
     
